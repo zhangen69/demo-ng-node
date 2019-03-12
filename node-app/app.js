25a4d57789e71a9ad42df05f27b86f69cd29a6bb
Jacob <zhangen69@gmail.com>
2019-03-12 14:01:06 +0800

    Update app.js
    - add `access-control-allow-methods`

--- node-app/app.js
+++ node-app/app.js
@@ -18,8 +18,9 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
 app.use('/api', productRoutes);
